95e56184a64a2e5c8c3d2bf1aaa2291bfa5aef4f
sareaewqef <43355517+sareaewqef@users.noreply.github.com>
2018-11-03 21:21:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`T-help .`,'https://www.twitch.tv/v5bz');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
